32bc0a4156bca27dd23da6fc534f8ce00a78ba9c
Whylgher Ferreira <whylgher@gmail.com>
2023-01-28 20:21:55 +0000

    store request

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class SkillController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json("Skill index");
+    }
+
+    public function show(Skill $skill)
+    {
+        return $skill;
+    }
+
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json('Skill Created');
+    }
+
+    public function update(StoreSkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validate([]));
+        return response()->json("Skill updated");
+    }
+
+    public function destroy(Skill $id)
+    {
+        $skill = Skill::find($id);
+        $skill->delete();
+    }
 }
